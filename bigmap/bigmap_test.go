2bbd36ca2ead02a9959e9657c3d48d82018a3001
weinandt <nick.weinandt@gmail.com>
2022-11-20 10:39:25 -0600

    added a test for 16 gig of mem

--- bigmap/bigmap_test.go
+++ bigmap/bigmap_test.go
@@ -3,6 +3,7 @@ package bigmap
 import (
 	"fmt"
 	"github.com/stretchr/testify/assert"
+	"math/rand"
 	"testing"
 )
 
@@ -25,4 +26,28 @@ func TestInsertAndGet(t *testing.T) {
 
 	// Assert
 	assert.Equal(t, expectedValue, returnedVal, "Strings should have matched.")
+}
+
+func generate500MBRandomString() string {
+	const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	b := make([]byte, 500*1024*1024)
+	for i := range b {
+		b[i] = letterBytes[rand.Intn(len(letterBytes))]
+	}
+	return string(b)
+}
+
+func TestInsert16Gigs(t *testing.T) {
+	bigString := generate500MBRandomString()
+	testMap, err := NewBigMap(16 * 1024 * 1024 * 1024)
+	if err != nil {
+		t.Fatal("could not create map.", err)
+	}
+
+	for i := 0; i < 16*2; i++ {
+		err := testMap.Add(fmt.Sprint(i), bigString)
+		if err != nil {
+			t.Fatal("Could not insert a big string into the map.", err)
+		}
+	}
 }
